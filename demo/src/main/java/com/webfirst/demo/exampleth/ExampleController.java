20fa179a0535a1582ea6df26a0b114ede12a9285
brightboost <info@brightboost.nl>
2022-11-08 17:00:27 +0100

    th conditions and loops

--- demo/src/main/java/com/webfirst/demo/exampleth/ExampleController.java
+++ demo/src/main/java/com/webfirst/demo/exampleth/ExampleController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class ExampleController {
 	@GetMapping("first-th")
-	public String getExampleTh(Model model, @RequestParam String name) {
+	public String getExampleTh(Model model, @RequestParam String name, @RequestParam String[] names) {
+		model.addAttribute("names", names);
 		model.addAttribute("name", name);
 		return "example";
 	}
